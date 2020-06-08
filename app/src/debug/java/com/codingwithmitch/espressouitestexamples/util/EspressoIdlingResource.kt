b2adb0eb75812bc58446970893a7ce173b6811ce
Nathan Meade <nathan.r.meade@gmail.com>
2020-06-08 18:41:16 -0400

    feat: Debug and release builds implemented

--- app/src/debug/java/com/codingwithmitch/espressouitestexamples/util/EspressoIdlingResource.kt
+++ app/src/debug/java/com/codingwithmitch/espressouitestexamples/util/EspressoIdlingResource.kt
@@ -3,7 +3,6 @@ package com.codingwithmitch.espressouitestexamples.util
 import androidx.test.espresso.idling.CountingIdlingResource
 
 object EspressoIdlingResource {
-
     private const val RESOURCE = "GLOBAL"
 
     @JvmField val countingIdlingResource = CountingIdlingResource(RESOURCE)
---
+++ app/src/release/java/com/codingwithmitch/espressouitestexamples/util/EspressoIdlingResource.kt
@@ -0,0 +1,12 @@
+package com.codingwithmitch.espressouitestexamples.util
+
+import androidx.test.espresso.idling.CountingIdlingResource
+
+object EspressoIdlingResource {
+
+    fun increment() {
+    }
+
+    fun decrement() {
+    }
+}
